456bac4672fc4392bfafc5fc34e60cd2690303d9
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-31 18:36:43 -0600

    five tests for updating records

--- test/create_test.js
+++ test/create_test.js
@@ -15,5 +15,4 @@ describe('Creating records', () => {
                 done();
             })
     });
-
 });
---
+++ test/update_test.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+const User = require('../src/user')
+
+describe('Updating records', () => {
+    let joe;
+
+    beforeEach((done) => {
+        joe = new User({name:"Joe"});
+        joe.save()
+            .then(()=>{
+                done();
+            })
+    })
+
+
+    function assertName(operation, done){
+        operation
+        .then( () =>
+            User.find({}) )
+            .then((usersReturned) => {
+                        assert(usersReturned.length === 1);
+                        assert(usersReturned[0].name ==='Alex');
+                        done();
+                    });
+    }
+
+    it('instance type using set and save', (done) => {
+        // console.log(joe);
+        //set used to change a property on a model instance
+        joe.set('name', 'Alex');
+        // console.log(joe); //returns object with 'Alex' as name
+
+        //MUST SAVE THIS CHANGE TO TEST IT
+        joe.save()
+            .then(()=> {
+                //passing in the empty object will fetch a list of ALL users in our table/collection
+                User.find({})
+                    .then((usersReturned)=>{
+                        assert(usersReturned.length ===1);
+                        assert(usersReturned[0].name ==='Alex');
+                        done();
+                    })
+
+            })
+    });
+
+
+    it('A model instance can update', (done) => {
+        //same as the test in the first 'it' block
+        assertName(joe.update({name: 'Alex'}), done);
+    });
+
+
+    it('A model class can update', (done) => {
+        //replace ALL records with name of Joe to have the name of Alex
+        assertName(User.update({ name: 'Joe' }, {name: 'Alex'}), done)
+    });
+
+
+
+    it('A model class can update one record', (done) => {
+        //update a SINGLE record by passing it a unique attribute. finds the first record that matches the gived condition and updates.
+        assertName(User.findOneAndUpdate({name: 'Joe'}, {name: 'Alex'}), done)
+    });
+
+
+    it('A model class can find a record with an Id and update', (done) => {
+        //update the record where id matches joe's id
+        assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done)
+    });
+
+    
+});
